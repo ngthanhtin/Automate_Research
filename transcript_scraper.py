eeb2e77ecd4692e32625cf497c8ff78b40f2c474
Thanh Tin Nguyen <thanhtinqn0903039105@gmail.com>
2024-12-19 14:13:51 -0600

    Update transcript_scraper.py

--- transcript_scraper.py
+++ transcript_scraper.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from os import listdir
 from os.path import isfile, join
-import random
-import cv2
 import numpy as np
 import pandas as pd
 import time
@@ -24,7 +22,6 @@ def transcript_scraper(html_file_path):
     with open(html_file_path, 'rb') as f:
         soup = BeautifulSoup(f.read(), 'html.parser')
     
-    
     # -----Get "Shape Media" image for this species-----
     sub_tag = soup.find('div', {"id":"subtitles-container", "class":"show-sub"})
     span_tags = sub_tag.find_all('span')
@@ -33,11 +30,13 @@ def transcript_scraper(html_file_path):
     vi_subs = []
 
     for i, span in enumerate(span_tags):
-        if i % 2 != 0:
-            continue
+        # if i % 2 != 0:
+        #     continue
         eng_sub_tag =  span.find_all('span', {"class":"js-textEn"})
         vi_sub_tag = span.find_all('small', {"class":"js-textVi"}) 
-
+        
+        if eng_sub_tag == [] or vi_sub_tag == []:
+            continue
         eng_text = eng_sub_tag[0].text
         vi_text = vi_sub_tag[0].text
         # print(eng_text)
@@ -55,6 +54,6 @@ def transcript_scraper(html_file_path):
     
 
 # %%
-transcript_scraper('/Users/tinnguyen/Downloads/FRIENDS-ss1-5.html')
+transcript_scraper('/Users/tinnguyen/Downloads/ss1_5.html')
 
 # %%
